76bd0e3591fdba36e45a5ab521b6798eda35984a
gdub-gif <glenn.wesenhagen@gmail.com>
2025-10-17 19:34:21 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  server: {
-    host: true,
-    port: 3000
-  },
+  plugins: [react()],
   esbuild: {
-    jsxInject: "import React from 'react'"
+    loader: 'jsx',
+    include: /src\/.*\.js$/,
+    exclude: [],
   }
-});
+})
